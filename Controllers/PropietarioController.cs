5279b9aca4373a8a4ecd62185a7b4d00674f5beb
Eourist <50326501+Eourist@users.noreply.github.com>
2021-03-25 17:25:46 -0300

    ABM Propietarios

---
+++ Controllers/PropietarioController.cs
@@ -0,0 +1,117 @@
+﻿using InmobiliariaSpartano.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InmobiliariaSpartano.Controllers
+{
+    public class PropietarioController : Controller
+    {
+        private readonly IConfiguration configuration;
+        private RepositorioPropietario repositorioPropietario;
+
+        public PropietarioController(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+            repositorioPropietario = new RepositorioPropietario(configuration);
+        }
+
+        // GET: TestController
+        public ActionResult Index()
+        {
+            List<Propietario> propietarios = repositorioPropietario.ObtenerTodos();
+            ViewData["Propietarios"] = propietarios;
+            //ViewBag.Personas = personas;
+            return View();
+        }
+
+        // GET: TestController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View(repositorioPropietario.ObtenerPorId(id));
+        }
+
+        // GET: TestController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: TestController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Propietario p)
+        {
+            try
+            {
+                repositorioPropietario.Alta(p);
+                return RedirectToAction(nameof(Index));
+            }
+            catch (Exception e)
+            {
+                ViewData["Error"] = e.Message;
+                return View();
+            }
+        }
+
+        // GET: TestController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View(repositorioPropietario.ObtenerPorId(id));
+        }
+
+        // POST: TestController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                Propietario p = new Propietario
+                {
+                    Id = Convert.ToInt32(collection["Id"]),
+                    Nombre = collection["Nombre"],
+                    Apellido = collection["Apellido"],
+                    Dni = collection["Dni"],
+                    Telefono = collection["Telefono"],
+                    Email = collection["Email"],
+                    Clave = collection["Clave"]
+                };
+                repositorioPropietario.Editar(p);
+                return RedirectToAction(nameof(Index));
+            }
+            catch (Exception e)
+            {
+                ViewData["Error"] = e.Message;
+                return View();
+            }
+        }
+
+        // GET: TestController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View(repositorioPropietario.ObtenerPorId(id));
+        }
+
+        // POST: TestController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                repositorioPropietario.Eliminar(id);
+                return RedirectToAction(nameof(Index));
+            }
+            catch (Exception e)
+            {
+                ViewData["Error"] = e.Message;
+                return View();
+            }
+        }
+    }
+}
---
+++ Models/RepositorioPropietario.cs
@@ -0,0 +1,144 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InmobiliariaSpartano.Models
+{
+    public class RepositorioPropietario : RepositorioBase
+    {
+        protected readonly string tabla;
+        public RepositorioPropietario (IConfiguration config) : base (config)
+        {
+            this.tabla = "Propietarios";
+        }
+
+        public int Eliminar(int id)
+        {
+            int res = -1;
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                string sql = $"DELETE FROM {tabla} WHERE Id = {id};";
+                using (var command = new SqlCommand(sql, connection))
+                {
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                    connection.Close();
+                    res = 1;
+                }
+            }
+            return res;
+        }
+
+        public int Editar(Propietario p)
+        {
+            int res = -1;
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                string sql = $"UPDATE {tabla} SET " +
+                    $"{nameof(Propietario.Nombre)} = @Nombre, " +
+                    $"{nameof(Propietario.Apellido)} = @Apellido, " +
+                    $"{nameof(Propietario.Dni)} = @Dni, " +
+                    $"{nameof(Propietario.Telefono)} = @Telefono, " +
+                    $"{nameof(Propietario.Email)} = @Email WHERE Id = {p.Id};";
+                using (var command = new SqlCommand(sql, connection))
+                {
+                    command.Parameters.AddWithValue("@Nombre", p.Nombre);
+                    command.Parameters.AddWithValue("@Apellido", p.Apellido);
+                    command.Parameters.AddWithValue("@Dni", p.Dni);
+                    command.Parameters.AddWithValue("@Telefono", p.Telefono);
+                    command.Parameters.AddWithValue("@Email", p.Email);
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                    connection.Close();
+                    res = 1;
+                }
+            }
+            return res;
+        }
+
+        public int Alta(Propietario p)
+        {
+            int res = -1;
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                string sql = $"INSERT INTO {tabla} " +
+                    $"({nameof(Propietario.Nombre)}, {nameof(Propietario.Apellido)}, {nameof(Propietario.Dni)}, {nameof(Propietario.Telefono)}, {nameof(Propietario.Email)}, {nameof(Propietario.Clave)}) " +
+                    $"VALUES (@Nombre, @Apellido, @Dni, @Telefono, @Email, @Clave); SELECT SCOPE_IDENTITY();";
+                using (var command = new SqlCommand(sql, connection))
+                {
+                    command.Parameters.AddWithValue("@Nombre", p.Nombre);
+                    command.Parameters.AddWithValue("@Apellido", p.Apellido);
+                    command.Parameters.AddWithValue("@Dni", p.Dni);
+                    command.Parameters.AddWithValue("@Telefono", p.Telefono);
+                    command.Parameters.AddWithValue("@Email", p.Email);
+                    command.Parameters.AddWithValue("@Clave", p.Clave);
+                    connection.Open();
+                    res = Convert.ToInt32(command.ExecuteScalar());
+                    p.Id = res;
+                    connection.Close();
+                }
+            }
+            return res;
+        }
+
+        public Propietario ObtenerPorId(int id)
+        {
+            Propietario res;
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                string sql = $"SELECT Id, Nombre, Apellido, Dni, Telefono, Email, Clave FROM {tabla} WHERE id = {id}";
+                using (SqlCommand command = new SqlCommand(sql, connection))
+                {
+                    connection.Open();
+                    SqlDataReader reader = command.ExecuteReader();
+                    reader.Read();
+                    res = new Propietario
+                    {
+                        Id = reader.GetInt32(0),
+                        Nombre = reader.GetString(1),
+                        Apellido = reader.GetString(2),
+                        Dni = reader.GetString(3),
+                        Telefono = reader.GetString(4),
+                        Email = reader.GetString(5),
+                        Clave = reader.GetString(6),
+                    };
+                    connection.Close();
+                }
+            }
+            return res;
+        }
+
+        public List<Propietario> ObtenerTodos()
+        {
+            List<Propietario> res = new List<Propietario>();
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                string sql = $"SELECT Id, Nombre, Apellido, Dni, Telefono, Email, Clave FROM {tabla}";
+                using (SqlCommand command = new SqlCommand(sql, connection))
+                {
+                    connection.Open();
+                    SqlDataReader reader = command.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        Propietario e = new Propietario
+                        {
+                            Id = reader.GetInt32(0),
+                            Nombre = reader.GetString(1),
+                            Apellido = reader.GetString(2),
+                            Dni = reader.GetString(3),
+                            Telefono = reader.GetString(4),
+                            Email = reader.GetString(5),
+                            Clave = reader.GetString(6),
+                        };
+                        res.Add(e);
+                    }
+                    connection.Close();
+                }
+            }
+            return res;
+        }
+    }
+}
